8d23e857fcaa92737b57ab380da0994e348f8830
Maddie <maddie.huebner@gmail.com>
2015-07-28 09:27:42 -0500

    Added name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,29 @@
+// $("#main").append(["Maddie"]);
+
+// [string].replace([old],[new]);
+
+
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+// var awesomeThoughts = "My name is Maddie and I am AWESOME!";
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// console.log(funThoughts);
+
+// $("#main").append(funThoughts);
+
+
+var name = "Maddie Huebner"
+var role = "Interaction Desinger"
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
+
