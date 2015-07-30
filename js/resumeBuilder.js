2516577376601e6a2c6ebe01be28761ec35c7fc1
Maddie <maddie.huebner@gmail.com>
2015-07-29 22:02:55 -0500

    Update objects to match Udacity requirements

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,32 +1,65 @@
-var work = {};
-work.position = "Interaction Designer";
-work.employer = "Brady Corporation";
-work.years = "1";
-work.city = "Milwaukee";
+var work = {
+	"job1": [
+		{
+			"employer": "Brady Corporation",
+			"title": "User Experience Specialist",
+			"location": "Milwaukee, WI",
+			"dates": "August 2014 - Present",
+			"description": "Integrate UX research and design into a cross-functional agile team, create wireframes/prototypes and utilize usability studies to quickly iterate, write basic HTML and CSS code for web-based projects."
+		}
+	],
+	"job2": [
+		{
+			"employer": "Brady Corporation",
+			"title": "User Experience Intern",
+			"location": "Milwaukee, WI",
+			"dates": "July 2013 - August 2014",
+			"description": "Collaborated with UX team members to conduct research projects utilizing tools such as Omniture, Google Analytics, usability testing, heuristic evaluations, and landing page optimization; provided website design update recommendations to stakeholders and created prototypes using interactive wireframe software."
+		}
+	],
+	"job3": [
+		{
+			"employer": "Brady Corporation",
+			"title": "Marketing Research Intern",
+			"location": "Milwaukee, WI",
+			"dates": "April 2012 - July 2013",
+			"description": "Organized multinational Customer Experience surveys from beginning to end including cleansing contact lists, executing survey launches, and analyzing customer feedback with Excel and SPSS to help Brady's leadership team make informed decisions. Increased action management completion rates by 40% year over year. Also conducted customer phone interviews, synthesized interview notes, and completed New Product Blueprinter training."
+		}
+	],
+	"job4": [
+		{
+			"employer": "TLS Behavioral Health",
+			"title": "Community Recovery Specialist",
+			"location": "Milwaukee, WI",
+			"dates": "June 2010 - April 2012",
+			"description": "Cared for adults in a group home by auditing and passing medication, assisting with housekeeping tasks, and ensuring that each resident's activities of daily living were accomplished. Also adapted quickly to new situations based on resident moods and symptoms of physical and mental illness, and modeled effective communication skills to promote independence and build a working relationship with residents whom I saw on a regular basis."
+		}
+	]
+}
 
 var projects = {
 	"project1": [
 		{
-			"name": "Mobile Website",
-			"year": 2015,
+			"title": "Mobile Website",
+			"dates": 2015,
 			"description": "Brady's mobile website allows users to quickly complete common tasks in a mobile-friendly environment including searching for products, viewing product information, and contacting Brady.",
-			"link": "http://m.us.amer.bradyid.com/"
+			"images": "http://m.us.amer.bradyid.com/"
 		}
 	],
 	"project2": [
 		{
-			"name": "Product Registraion Form",
-			"year": "2015",
+			"title": "Product Registraion Form",
+			"dates": "2015",
 			"description": "Brady's product registration page allows users to select their product in a visually engaging way and register it with the company.",
-			"link": "http://www.us.amer.bradyid.com/en-us/forms/register-your-product/product-registration/step-1"
+			"images": "http://www.us.amer.bradyid.com/en-us/forms/register-your-product/product-registration/step-1"
 		}
 	],
 	"project3": [
 		{
-			"name": "Technical Support Form",
-			"year": "2015",
+			"title": "Technical Support Form",
+			"dates": "2015",
 			"description": "Brady's technical support form allows users to contact Brady for product information and troubleshooting.",
-			"link": "http://www.us.amer.bradyid.com/en-us/forms/technical-support/technical-support"
+			"images": "http://www.us.amer.bradyid.com/en-us/forms/technical-support/technical-support"
 		}
 	]
 }
@@ -35,31 +68,43 @@ var bio = {
 	"name" : "Maddie",
 	"role" : "Interaction Designer",
 	"contacts" : {
-		"phone" : "(312)361-4674"
+		"mobile" : "(312)361-4674",
+		"email" : "maddie.huebner@gmail.com",
+		"github" : "huebnema",
+		"location" : "Milwaukee, WI"
 	},
 	"welcomeMessage" : "Welcome to my resume!",
 	"skills" : [
 		"Interaction Design", "Usability Research", "Prototyping"
 		],
 	"bioPhoto" : "images/me.jpg"
-};
+}
 
 var education = {
 	"schools": [
 		{
 			"name": "University of Wisconsin - Milwaukee",
-			"city": "Milwaukee, WI, US",
+			"location": "Milwaukee, WI, United States",
 			"degree": "BBA",
-			"major": "Marketing",
+			"majors": "Marketing",
 			"minor": "Anthropology",
-			"graduationYear": 2014
+			"dates": "September 2009 - August 2014",
+			"url": "http://www4.uwm.edu/"
+		},
+		{
+			"name": "Novancia Business School",
+			"location": "Paris, France",
+			"dates": "May 2014 - June 2014",
+			"program": "Study Abroad Program:  Immersion in European Business",
+			"url": "http://www.novancia.fr/?lang=en"
 		}
 	],
-	"onlinePrograms": [
+	"onlineCourses": [
 		{
 			"title": "Front-end Nanodegree",
 			"school": "Udacity",
-			"dates": 2015
+			"dates": 2015,
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
 		}
 	]
 }
